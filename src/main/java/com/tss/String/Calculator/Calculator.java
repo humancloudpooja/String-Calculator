80903b0b160cf621766ecb3b268ad8de552b0f0c
Pooja Narale <pooja.narale@humancloud.co.in>
2024-10-25 16:51:13 +0530

    Test case pass to add Empty String

---
+++ src/main/java/com/tss/String/Calculator/Calculator.java
@@ -0,0 +1,18 @@
+package com.tss.String.Calculator;
+
+public class Calculator {
+
+    public static int add(String text)
+    {
+        if(!text.equals(""))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+}
