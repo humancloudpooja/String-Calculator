ff8b2dbf63ede532fd40f2283ab0ad8a8b185f05
Pooja Narale <pooja.narale@humancloud.co.in>
2024-10-25 16:42:48 +0530

    Test Case fail for Empty String

--- src/test/java/com/tss/String/Calculator/StringCalculatorApplicationTests.java
+++ src/test/java/com/tss/String/Calculator/StringCalculatorApplicationTests.java
@@ -3,11 +3,14 @@ package com.tss.String.Calculator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class StringCalculatorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testEmptyString()
+	{
+		assertEquals(0,Calculator.add(""));
 	}
-
 }
